a926060fe8791576a779a30603ea2cd3f8cf9af6
Gabriel Mendez <gabrielenrique904@gmail.com>
2026-07-01 21:39:45 +0200

    Revert "Login santiago merge"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 import "./i18n";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
